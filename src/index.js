eb5c69575550b00f9c75f2a4ca575561ba832fe3
devendra-alt <devendramulewa171@gmail.com>
2023-08-28 00:31:32 +0530

    Add generativeAI route in index.js

--- src/index.js
+++ src/index.js
@@ -1,13 +1,14 @@
 const express = require('express');
+const generativeAi = require('./router/generativeAi');
 
 const app = express();
 const port = process.env.PORT || 3000;
 
 app.get('/', (request, respond) => {
-  respond.status(200).json({
-    message: 'Hello from GenAI!',
-  });
+  respond.status(200).send('GenAI Home!');
 });
+
+app.use('/generativeAI', generativeAi);
 
 app.listen(port, () => {
   console.log(`gen-ai-ext server running on port ${port}`);
